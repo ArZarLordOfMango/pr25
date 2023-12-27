fef301c58b455ef0a8f87191db1cec7ec9319315
ArZarLordOfMango <arzarchannel@gmail.com>
2023-12-27 11:39:55 +0100

    е

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
 function generateProducts() {
   const productList = document.getElementById('product-list');
 
-  fetch('./products.json')
+  // Загрузка продуктов из внешнего JSON файла
+  fetch('products.json')
       .then(response => response.json())
       .then(products => {
           products.forEach(product => {
-              const productDiv = document.createElement('div');
-              productDiv.className = 'product';
-              productDiv.style.backgroundImage = `url('${product.image}')`;
+              const productLink = document.createElement('a');
+              productLink.className = 'product';
+              productLink.href = '#'; // Замените '#' на реальную ссылку
 
-              // Добавляем название и цену продукта
+              const productImage = document.createElement('img');
+              productImage.src = product.image;
+              productImage.alt = product.name;
+
               const productInfo = document.createElement('div');
               productInfo.className = 'product-info';
               productInfo.innerHTML = `<p>${product.name}</p><p>${product.price}</p>`;
 
-              productDiv.appendChild(productInfo);
-              productList.appendChild(productDiv);
+              productLink.appendChild(productImage);
+              productLink.appendChild(productInfo);
+              productList.appendChild(productLink);
           });
       })
       .catch(error => console.error('Error loading products:', error));
 }
 
+// Функция прокрутки продуктов
 function scrollProducts() {
   const productList = document.getElementById('product-list');
   productList.scrollBy(200, 0);
 }
 
+// Вызываем генерацию продуктов при загрузке страницы
 window.onload = generateProducts;
